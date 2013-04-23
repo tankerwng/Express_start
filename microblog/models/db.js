c46a6e01cc3d618e8a4a8d87c6ff7b67cbf339c3
tanker <tankerwng@gmail.com>
2013-04-23 15:24:50 +0800

    remove some unuse files

--- microblog/models/db.js
+++
@@ -1,4 +0,0 @@
-var settings = require('../settings');
-var mongoose = require('mongoose');
-var connect_str = 'mongodb://'+settings.host+'/'+settings.db;
-module.exports = mongoose.connect(connect_str);
